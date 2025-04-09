22ff8efd5e30809001b872287b9e5e3fb5a4a100
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-04-09 07:15:21 +0200

    Update API base URL to production environment

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 // utils/api.js
 
-// const API_BASE_URL = 'https://aokaze-sushi.vercel.app/api';
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = 'https://aokaze-sushi.vercel.app/api';
+// const API_BASE_URL = "http://localhost:5000/api";
 
 /**
  * Get the authentication token from localStorage
