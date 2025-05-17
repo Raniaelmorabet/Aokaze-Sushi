64ecdde02af146770037baf6e62a380e6fb7a486
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2025-05-17 06:47:48 +0300

    feat: add useLocalStorage hook for managing local storage state

---
+++ hooks/use-local-storage.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+export default function useLocalStorage(key: string) {
+  const [value, setValue] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setValue(localStorage.getItem(key));
+    }
+  }, [key]);
+
+  return value;
+}
+
